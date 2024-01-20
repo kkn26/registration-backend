e36a50470bdc50428ae313477ba9e748c52034cf
cocomay <khai.nyunt@outlook.com>
2024-01-20 07:57:13 -0800

    updated registrationController

--- src/main/java/com/tmc/registration/controller/RegistrationController.java
+++ src/main/java/com/tmc/registration/controller/RegistrationController.java
@@ -3,9 +3,11 @@ package com.tmc.registration.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tmc.registration.jpa.User;
@@ -17,6 +19,11 @@ public class RegistrationController {
 	
 	@Autowired
 	UserRepository userRepository; 
+	
+	@GetMapping("/")
+	public @ResponseBody ResponseEntity<String> get() {
+	    return new ResponseEntity<String>("GET Response", HttpStatus.OK);
+	}
 	
 	@PostMapping("/create")
 	public ResponseEntity<User> createUser(@RequestBody User user) {
