d7cbcf7f26babbe01675dfadac768f7b81a96912
cocomay <khai.nyunt@outlook.com>
2024-01-21 21:21:03 -0800

    fixed for security error

--- src/main/java/com/tmc/registration/WebConfig.java
+++ src/main/java/com/tmc/registration/WebConfig.java
@@ -3,31 +3,24 @@ package com.tmc.registration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @Configuration
-// public class WebConfig implements WebMvcConfigurer {
+ @Configuration
+ public class WebConfig implements WebMvcConfigurer {
 	
-// 	@Value("${allowed.origins}")
-//     private String allowedOrigins;
+ 	@Value("${allowed.origins}")
+     private String allowedOrigins;
 	
-//     @Override
-//     public void addCorsMappings(CorsRegistry registry) {
-//     	// Add your Angular app URL 
-//         registry.addMapping("/**")
-// 	        .allowedOrigins(allowedOrigins.split(","))
-// 	        .allowedMethods("GET", "POST", "PUT", "DELETE")
-// 	        .allowedHeaders("*");
-//     }
+     @Override
+     public void addCorsMappings(CorsRegistry registry) {
+     	// Add your Angular app URL 
+         registry.addMapping("/**")
+ 	        .allowedOrigins(allowedOrigins.split(","))
+ 	        .allowedMethods("GET", "POST", "PUT", "DELETE")
+ 	        .allowedHeaders("*");
+     }
  
-// }
+ }
 
-@Configuration
-@EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
-    }
-}
+ 
