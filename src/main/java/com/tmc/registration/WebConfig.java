9f80e2ca1f3fa4fa093f45b9065a2679e1530f1c
cocomay <khai.nyunt@outlook.com>
2024-01-22 23:38:25 -0800

    CORS

--- src/main/java/com/tmc/registration/WebConfig.java
+++ src/main/java/com/tmc/registration/WebConfig.java
@@ -11,14 +11,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  	@Value("${allowed.origins}")
      private String allowedOrigins;
 	
-     @Override
-     public void addCorsMappings(CorsRegistry registry) {
-     	// Add your Angular app URL 
-         registry.addMapping("/**")
- 	        .allowedOrigins(allowedOrigins.split(","))
- 	        .allowedMethods("GET", "POST", "PUT", "DELETE")
- 	        .allowedHeaders("*");
-     }
+    //  @Override
+    //  public void addCorsMappings(CorsRegistry registry) {
+    //  	// Add your Angular app URL 
+    //      registry.addMapping("/**")
+ 	//         .allowedOrigins(allowedOrigins.split(","))
+ 	//         .allowedMethods("GET", "POST", "PUT", "DELETE")
+ 	//         .allowedHeaders("*");
+    //  }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://kind-pebble-0802ad31e.4.azurestaticapps.net")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
  
  }
 
